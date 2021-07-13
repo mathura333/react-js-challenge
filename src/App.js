543200743fdf3aa5d998e45f09b488dba0e7055c
mathura333 <mathuradas333@gmail.com>
2021-07-13 13:52:22 +0530

    X out of Y vaccinated Header added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import dayjs from 'dayjs';
 import PieChart from './components/pieChart';
 import './App.css';
 function App() {
-  const [currentDate, setCurrentDate] = useState(dayjs()); // Current Date Fetch from current_date.json in public/data
+  const [currentDate, setCurrentDate] = useState(dayjs('2021-04-28')); // Current Date Fetch from current_date.json in public/data
   const [vaccineDates, setVaccineDates] = useState([]); // List Of Vaccine Dates Fetch from vaccine_dates.json in public/data
+  const [err, setErr] = useState('');
+  const [totalPeople, setTotalPeople] = useState(0);
+  const [currentPeople, setCurrentPeople] = useState(0);
 
   useEffect(() => {
+    const getVaccinationDatesArr = () => {
+      fetch('/data/vaccine_dates.json')
+        .then((response) => response.json())
+        .then((data) => {
+          const vaccinationDates = data.map(
+            ({ vaccination_date }) => vaccination_date
+          );
+          setTotalPeople(data.length);
+          setVaccineDates(vaccinationDates);
+        })
+        .catch((error) => setErr(error.message));
+    };
+    getVaccinationDatesArr();
     // Use fetch() to fetch requited data from public/data
-    fetch('');
   }, []);
+
+  useEffect(() => {
+    setCurrentPeople(
+      vaccineDates.filter(
+        (vaccinationDate) =>
+          vaccinationDate === currentDate.format('YYYY-MM-DD')
+      ).length
+    );
+  }, [currentDate, vaccineDates]);
 
   const incrementDate = () => {
     setCurrentDate((currentDate) => currentDate.add(1, 'day'));
@@ -18,11 +42,6 @@ function App() {
   const decrementDate = () => {
     setCurrentDate((currentDate) => currentDate.subtract(1, 'day'));
   };
-
-  useEffect(() => {
-    console.log(currentDate);
-    console.log(currentDate);
-  }, [currentDate]);
 
   return (
     <div className="App">
@@ -33,6 +52,7 @@ function App() {
         <button onClick={decrementDate}>-</button>{' '}
         {/* Set Current Date to drevious date on click  */}
       </div>
+      <div>{`${currentPeople} out of ${totalPeople} have been vaccinated`}</div>
       <div className="chart">
         {/* Update the following Component to display pie chart with proper data, alignment and colors */}
         <PieChart data={[60, 40]} />
